9dd95c8b5164af229cdad97cdd153cb06884479e
pc-aide <36453201+pc-aide@users.noreply.github.com>
2020-08-08 14:49:09 -0400

    Create APP 2020.bash

---
+++ Ubuntu/APP/APP 2020.bash
@@ -0,0 +1,15 @@
+: '
+-----------------------------------------------------------------------------------
+  man 
+  
+  
+-----------------------------------------------------------------------------------
+'
+
+#Write (pencil)
+wget http://www.styluslabs.com/download/write-tgz
+
+#OBS
+sudo add-apt-repository ppa:obsproject/obs-studio
+sudo apt-get update
+sudo apt-get install obs-studio -y

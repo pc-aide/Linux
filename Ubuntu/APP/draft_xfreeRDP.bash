249d395bd320f3fcaffb4095bc3c986f0dd8e042
pc-aide <36453201+pc-aide@users.noreply.github.com>
2020-10-01 08:56:47 -0400

    Update draft_xfreeRDP.bash

--- Ubuntu/APP/draft_xfreeRDP.bash
+++ Ubuntu/APP/draft_xfreeRDP.bash
@@ -20,3 +20,6 @@ xfreerdp /v:192.168.0.138 /u:adminS /monitors:1 /f
  
 #Sound | https://imgur.com/crHJYB1
 xfreerdp /v:<IP> /u:<User> /f /d:<Domain> /sound /microphone:sys:alsa #rdp
+
+# 
+xfreerdp /v:15.237.60.56 /size:1440x900 /u:administrator  /monitors:2 #rdp-W10
